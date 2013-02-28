3fc3d9da0897589307bc147102ee11e2fb7cae23
ChanMin Kim <kcm1700@gmail.com>
2013-02-28 09:50:16 +0900

    정확한 링크 몰라서; 일단은 파싱해서 링크 있으면 따라 들어가보도록 함.

---
+++ auto_hurumau.py
@@ -0,0 +1,22 @@
+import urllib2
+import time
+import sys
+from BeautifulSoup import BeautifulSoup
+cookie = raw_input('input konmai cookie: ')
+opener = urllib2.build_opener()
+opener.addheaders.append(('Cookie', cookie))
+
+while True:
+	try:
+		page = opener.open('http://p.eagate.573.jp/game/bemani/cafedetran/p/')
+		soup = BeautifulSoup(page)
+		hurumau_part = soup.find(id="action_btn_container")
+		print hurumau_part
+		for link in hurumau_part.findAll('a'):
+			print link
+			newurl = link.get('href')
+			opener.open(newurl)
+		time.sleep(60)
+	except Exception, e:
+		print sys.exc_info()[0], e
+		time.sleep(240)
