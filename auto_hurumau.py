9275e6bbc194670dc5bda2e8149ca110491371e8
ChanMin Kim <kcm1700@gmail.com>
2013-03-01 01:42:07 +0900

    a가 아니라 form으로 post하는 것을 보니 파싱해서 전송해야겠다.

--- auto_hurumau.py
+++ auto_hurumau.py
@@ -1,3 +1,4 @@
+import urllib
 import urllib2
 import time
 import sys
@@ -13,10 +14,21 @@ while True:
 		soup = BeautifulSoup(page)
 		hurumau_part = soup.find(id="action_btn_container")
 		print hurumau_part
-		for link in hurumau_part.findAll('a'):
+		for link in hurumau_part.findAll('form'):
 			print link
-			newurl = link.get('href')
-			opener.open(newurl)
+			newurl = link.get('action')
+			if newurl[0] == '/':
+				newurl = 'http://p.eagate.573.jp' + newurl
+			else:
+				newurl = 'http://p.eagate.573.jp/game/bemani/cafedetran/p/' + newurl
+			values = {}
+			for inp in link.findAll('input'):
+				if inp.get('type') == 'hidden':
+					values[inp.get('name')] = inp.get('value')
+			data = urllib.urlencode(values)
+			req = urllib2.Request(newurl, data)
+			response = urllib2.urlopen(req)
+			response.read()
 		time.sleep(60)
 	except Exception, e:
 		print sys.exc_info()[0], e
