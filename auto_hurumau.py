1a319f59aa622f42c812ea2fdaf563e82f101d1d
ChanMin Kim <kcm1700@gmail.com>
2013-03-05 01:25:03 +0900

    found all addresses :)

--- auto_hurumau.py
+++ auto_hurumau.py
@@ -10,25 +10,12 @@ opener.addheaders.append(('Host', 'p.eagate.573.jp'))
 
 while True:
 	try:
-		page = opener.open('http://p.eagate.573.jp/game/bemani/cafedetran/p/')
+		page = opener.open('http://p.eagate.573.jp/game/bemani/cafedetran/p/index.html?c=1')
+		time.sleep(10)
+		opener.open('http://p.eagate.573.jp/game/bemani/cafedetran/p/get.html')
 		soup = BeautifulSoup(page)
 		hurumau_part = soup.find(id="action_btn_container")
 		print hurumau_part
-		for link in hurumau_part.findAll('form'):
-			print link
-			newurl = link.get('action')
-			if newurl[0] == '/':
-				newurl = 'http://p.eagate.573.jp' + newurl
-			else:
-				newurl = 'http://p.eagate.573.jp/game/bemani/cafedetran/p/' + newurl
-			values = {}
-			for inp in link.findAll('input'):
-				if inp.get('type') == 'hidden':
-					values[inp.get('name')] = inp.get('value')
-			data = urllib.urlencode(values)
-			req = urllib2.Request(newurl, data)
-			response = urllib2.urlopen(req)
-			response.read()
 		time.sleep(150)
 	except Exception, e:
 		print sys.exc_info()[0], e
